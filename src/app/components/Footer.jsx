46cfd070e8bf45b680604058715c0539d448e266
sangam verma <sangamverma955911@gmail.com>
2025-03-20 11:57:12 +0530

    footer final

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -22,19 +22,19 @@ const Footer = () => {
     <div className='sm:px-14 md:px-20 lg:px-32 xl:px-40 px-10 py-8 bg-gradient-to-b from-white to-[#FFEBE0]'>
       <div className={`flex flex-col sm:flex-row sm:justify-between sm:items-center`}>
         <h2 className={`text-[28px] ${bricolageGrotesque.className} leading-tight`}>Get started today or schedule<br /> a quick 15 min demo</h2>
-        <div className='mt-5 sm:mt-0'><button className={` ${outfit.className} px-8 py-3 bg-[#FD5F03] cursor-pointer rounded-md text-white text-xl font-bold`}>BOOK A DEMO</button></div>
+        <div className='mt-5 sm:mt-0'><button className={` ${outfit.className} px-8 py-3 bg-[#FD5F03] cursor-pointer rounded-md text-white text-md sm:text-xl font-bold`}>BOOK A DEMO</button></div>
       </div>
       <div className='w-[100%] h-[0.8px] bg-[#fcf1e4] relative my-4'></div>
-      <div className='flex sm:flex-row justify-between sm:items-center my-4 py-2'>
-        <div className='flex'>
+      <div className='flex flex-col sm:flex-row justify-between sm:items-center my-4 py-2'>
+        <div className='flex mb-5 sm:mb-0 justify-center sm:justify-normal'>
           <img src={logo.src} alt={`${logo}`} width={30} height={30} className="mr-2" />
           <h1 className={`hover:cursor-pointer text-[#FD5F03] font-Poppins font-semibold text-[4vh] leading-[110%] ${poppins.className}`}>
             AppStorys
           </h1>
         </div>
-        <div className='flex'>
+        <div className='flex justify-center sm:justify-normal'>
           <p >
-            <Image src={linkedin_icon.src} width={25} height={20} alt='pedin' className='mx-3' />
+            <Image src={linkedin_icon.src} width={25} height={20} alt='pedin' className='mr-3' />
           </p>
           <p >
             <Image src={insta_icon.src} width={25} height={20} alt='Instagram' className='mx-3' />
@@ -50,7 +50,7 @@ const Footer = () => {
       <div className='w-[100%] h-[0.8px] bg-[#fcf1e4] relative my-2'></div>
       <div className='grid grid-cols-1 sm:grid-cols-4 my-4 mt-6'>
         <div className='flex justify-center sm:justify-normal'>
-          <div className='flex flex-col text-center sm:text-left'>
+          <div className='flex flex-col text-center sm:text-left mb-5 sm:mb-0'>
             <h3 className={`${bricolageGrotesque.className} capitalize`}>Integratable growth Tools</h3>
             <p className={`${outfit.className} font-extralight capitalize text-[#606060] hover:text-[#000000] text-[14px]`}>Stories</p>
             <p className={`${outfit.className} font-extralight capitalize text-[#606060] hover:text-[#000000] text-[14px]`}>reels/Short videos</p>
@@ -70,7 +70,7 @@ const Footer = () => {
         </div>
         <div className='flex justify-center'>
           <div className='flex flex-col justify-between'>
-            <div className='flex flex-col text-center sm:text-left'>
+            <div className='flex flex-col text-center sm:text-left mb-5 sm:mb-0'>
               <h3 className={`${bricolageGrotesque.className} capitalize  `}>Our SDKs</h3>
               <p className={`${outfit.className} font-extralight text-[#606060] hover:text-[#000000] text-[14px] `}>iOS</p>
               <p className={`${outfit.className} font-extralight capitalize text-[#606060] hover:text-[#000000] text-[14px]`}>android</p>
@@ -81,7 +81,7 @@ const Footer = () => {
               <p className={`${outfit.className} font-extralight capitalize text-[#606060] hover:text-[#000000] text-[14px]`}>wordpress</p>
               <p className={`${outfit.className} font-extralight capitalize text-[#606060] hover:text-[#000000] text-[14px]`}>shopify</p>
             </div>
-            <div className='flex flex-col text-center sm:text-left'>
+            <div className='flex flex-col text-center sm:text-left mb-5 sm:mb-0'>
               <h3 className={`${bricolageGrotesque.className} capitalize  `}>resources</h3>
               <p className={`${outfit.className} font-extralight capitalize text-[#606060] hover:text-[#000000] text-[14px]`}>blog</p>
               <p className={`${outfit.className} font-extralight capitalize text-[#606060] hover:text-[#000000] text-[14px]`}>inspiration gallery</p>
@@ -91,7 +91,7 @@ const Footer = () => {
         </div>
 
         <div className='flex justify-center'>
-          <div className='flex flex-col text-center sm:text-left'>
+          <div className='flex flex-col text-center sm:text-left mb-5 sm:mb-0'>
             <h3 className={`${bricolageGrotesque.className} capitalize  `}>Integrations</h3>
             <p className={`${outfit.className} font-extralight text-[#606060] hover:text-[#000000] text-[14px] capitalize`}>cleverTap</p>
             <p className={`${outfit.className} font-extralight text-[#606060] hover:text-[#000000] text-[14px]`}>MoEngage</p>
@@ -102,7 +102,7 @@ const Footer = () => {
         </div>
         <div className='flex sm:justify-end justify-center'>
           <div className='flex flex-col justify-between'>
-            <div className='flex flex-col text-center sm:text-left'>
+            <div className='flex flex-col text-center sm:text-left mb-5 sm:mb-0'>
               <h3 className={`${bricolageGrotesque.className} capitalize  `}>security</h3>
               <p className={`${outfit.className} font-extralight text-[#606060] hover:text-[#000000] text-[14px] capitalize`}>SOC 2 verified</p>
               <p className={`${outfit.className} font-extralight text-[#606060] hover:text-[#000000] text-[14px] capitalize`}>encrypted</p>
