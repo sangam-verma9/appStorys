d8a3dd3446a3f528c1c4fbfdf5e1437250c00235
Pratyush0026 <113766134+Pratyush0026@users.noreply.github.com>
2025-04-02 12:22:52 +0530

    core features section developed

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@ import Image from "next/image";
 import LandingPage from "./components/Landingpage/LandingPage";
 import Whyus from "./components/Whyus/Whyus";
 import HowItWorks from "./components/Howitworks/HowItWorks";
+import CoreFeatures from "./components/CoreFeatures/CoreFeatures";
 
 export default function Home() {
   return (
     <>
       {/* <LandingPage /> */}
       <Whyus />
+      <CoreFeatures/>
       {/* <HowItWorks/> */}
     </>
   );
