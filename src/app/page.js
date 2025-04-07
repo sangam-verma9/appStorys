6a76800133908a69937639d411a36d23e042f00b
Pratyush0026 <113766134+Pratyush0026@users.noreply.github.com>
2025-04-07 17:37:37 +0530

    story page added

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import LandingPage from "./components/Landingpage/LandingPage";
+// import LandingPage from "./components/Landingpage/LandingPage";
 import Whyus from "./components/Whyus/Whyus";
 import HowItWorks from "./components/Howitworks/HowItWorks";
 import CoreFeatures from "./components/CoreFeatures/CoreFeatures";
