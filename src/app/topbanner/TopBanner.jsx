a745c38e9a4817a5140128c941d9a9e61201ad2d
sangam verma <sangamverma955911@gmail.com>
2025-05-26 13:24:17 +0530

    top banner link added

--- src/app/topbanner/TopBanner.jsx
+++ src/app/topbanner/TopBanner.jsx
@@ -6,9 +6,10 @@ const TopBanner = () => {
     return (
         <>
             <div className={`w-full  h-[50px] flex bg-[#FD5F03] py-3 z-50 justify-center items-center`}>
-                <img src={doller_icon.src} alt="doller" height={25} width={25} className='md:mr-2 mr-[3px] scale-[0.8] md:scale-[1]' />
-                <p className='text-white font-semibold mr-1 md:text-xl text-[12px]'>We’ve raised $5M to power the next journey of growth</p>
-                <Link href={"https://www.prweb.com/releases/appstorys-raises-5-million-in-funding-to-accelerate-global-expansion-and-empower-businesses-with-growth-tools-302379955.html"}>
+                <Link href={"https://www.prweb.com/releases/appstorys-raises-5-million-in-funding-to-accelerate-global-expansion-and-empower-businesses-with-growth-tools-302379955.html"}
+                    className='flex'>
+                    <img src={doller_icon.src} alt="doller" height={25} width={25} className='md:mr-2 mr-[3px] scale-[0.8] md:scale-[1]' />
+                    <p className='text-white font-semibold mr-1 md:text-xl text-[12px]'>We’ve raised $5M to power the next journey of growth</p>
                     <GoArrowUpRight className='md:w-[25px] md:h-[25px] w-[15px] h-[15px] text-white md:-mb-2' />
                 </Link>
             </div>
